1df8314ad379ca18fb43514fbf78b4ce4f80c3c0
Pwu-Edu7 <58974222+Pwu-Edu7@users.noreply.github.com>
2021-01-24 22:09:49 +0100

    ajout fig5 et fig6

--- DSP_rectangulaire.m
+++ DSP_rectangulaire.m
@@ -1,7 +1,6 @@
 
-function [DSP_signal,f] = DSP_rectangulaire(Nb_points,Fe,signal)
+function [DSP_signal,f] = DSP_rectangulaire(Nb_points,Fe,signal,Translation)
 
 DSP_signal = periodogram(signal,[],Nb_points,'centered');
-%N = Nb_points;
-f = (-Nb_points/2:Nb_points/2-1)*(Fe/Nb_points);
+f = (-Nb_points/2:Nb_points/2-1)*(Fe/Nb_points) + Translation;
 end
--- Projet.m
+++ Projet.m
@@ -12,6 +12,7 @@ Ts = 1/300; % s/bits, le débit souhaité max est de 300 bits/seconde
 Ns = floor(Ts/Te); % échantillons/bits, on prend la partie entière, Ts = Ns*Te
 Nb_bits = 100; % Nombre de bits du signal émis
 Nb_echantillons = Nb_bits*Ns; % Nombre d'échantillons
+Null = 2000;
 
 donnees = randi(0:1, Nb_bits, 1); % Génération de Nb_bits 0 ou 1 de manière aléatoire
 NRZ = zeros(Nb_echantillons,1); % Initialisation du signal NRZ avec Nb_echantillons echantillons
@@ -19,8 +20,9 @@ T = ([0:Nb_echantillons-1]*Te)'; % Le signal est tracé de 0 à (Nb_echantillons
                                  % avec un pas de Te
 
                                  
-%% Modem de fréquence - Démodulation par filtrage
+%% 3.Modem de fréquence - Démodulation par filtrage
         
+
 %3.1 Construction du signal modulé en fréquence
 
 %3.1.1.1 Génération du signal NRZ 
@@ -37,7 +39,7 @@ ylabel('NRZ(t)');
 title('Signal NRZ(t) en fonction du temps');
 
 %3.1.1.3 Tracé de la DSP de NRZ théorique et expérimentale
-[DSP_NRZ_Th,f] = DSP_rectangulaire(Nb_echantillons,Fe,NRZ);
+[DSP_NRZ_Th,f] = DSP_rectangulaire(Nb_echantillons,Fe,NRZ,0);
 figure; % figure 2
 subplot(211);
 semilogy(f,DSP_NRZ_Th);
@@ -68,14 +70,20 @@ x_module = (1 - NRZ).*Cos0 + NRZ.*Cos1;
 figure; % figure 3
 plot(T,x_module);
 ylim([-1.5 1.5]);
+xlim([0.05 0.08]);
 xlabel('t en s');
 ylabel('x(t)');
 title('Signal x(t) en fonction de t');
 
 %3.1.2.3 Calcul de la DSP de x(t) en fonction de la DSP de NRZ
+% DSP_NRZ_plus_F1 = DSP_rectangulaire(Nb_echantillons,Fe,NRZ,F1);
+% DSP_NRZ_plus_F0 = DSP_rectangulaire(Nb_echantillons,Fe,NRZ,F0);
+% DSP_NRZ_moins_F1 = DSP_rectangulaire(Nb_echantillons,Fe,NRZ,-F1);
+% DSP_NRZ_moins_F0 = DSP_rectangulaire(Nb_echantillons,Fe,NRZ,-F0);
+% DSP_X_Exp = (dirac(f-F1) + dirac(f+F1) - dirac(f-F0) - dirac(f-F1))*DSP_NRZ_Exp/4;
 
 %3.1.2.4 Tracé de la DSP de x(t) théorique et expérimentale
-DSP_X_Th = DSP_rectangulaire(Nb_echantillons,Fe,x_module);
+DSP_X_Th = DSP_rectangulaire(Nb_echantillons,Fe,x_module,0);
 figure; % figure 4
 subplot(211);
 %ylim([1e-04 10]);
@@ -85,35 +93,75 @@ xlabel('f en Hz');
 ylabel('DSP X Th(f)');
 title('DSP X théorique en fonction de f');
 
+subplot(212);
+%ylim([1e-04 10]);
+%xlim([-6000 6000]);
+% semilogy(f,DSP_X_Exp);
+% xlabel('f en Hz');
+% ylabel('DSP X Exp(f)');
+% title('DSP X expérimental en fonction de f');
 
-S_x = abs(fft(x_module).^2)/length(x_module);
-figure;
-subplot(411);
-semilogy(S_x);
-subplot(412);
-pwelch(x_module);
+% S_x = abs(fft(x_module).^2)/length(x_module);
+% figure; 
+% subplot(411);
+% semilogy(S_x);
+% subplot(412);
+% pwelch(x_module);
+
+%3.2 Canal de transmission à bruit additif, blanc et Gaussien
+Px = mean(abs(x_module).^2);
+SNR = 10;
+sigma = sqrt(Px*10^(-SNR/10));
+bruit = sigma*randn(1,Nb_echantillons);
+
+figure; % figure 5 
+plot(T,bruit);
+ylim([-1.5 1.5]);
+xlabel('Temps (secondes)');
+ylabel('bruit');
+title('Tracé du bruit en fonction du temps');
+
+x_bruite = x_module + transpose(bruit);
+figure; % figure 6 
+subplot(211);
+plot(T,x_module);
+ylim([-1.5 1.5]);
+xlim([0.05 0.06]);
+xlabel('t en s');
+ylabel('x(t) sans bruit');
+title('Signal x(t) avant ajout du bruit en fonction de t');
+
+subplot(212);
+plot(T,x_bruite);
+ylim([-1.5 1.5]);
+xlim([0.05 0.06]);
+xlabel('Temps (secondes)');
+ylabel('x bruite');
+title('Tracé du signal x bruite en fonction du temps');
+
+%3.3 Démodulation par filtrage
 
 %3.3.1 Filtre passe-bas
-ordre = 60;
-g = Fe*(1:length(x_module))/length(x_module);
-fc = 2500; %Hz
-intervalle = (-ordre*Te:Te:ordre*Te);
-
-figure;
-h = (2*fc/Fe)*sinc(2*fc*intervalle);
-H_passe_bas = abs(fftshift(fft(h)));
-Freq = (-(2*ordre+1)/2:(2*ordre+1)/2-1)*(Fe/(2*ordre+1));
-z = filter(H_passe_bas,[1],x_module);
-Z = abs(fft(z));
-plot(Freq,H_passe_bas);
+% ordre = 60;
+% g = Fe*(1:length(x_module))/length(x_module);
+% fc = 2500; %Hz
+% intervalle = (-ordre*Te:Te:ordre*Te);
+% 
+% figure;
+% h = (2*fc/Fe)*sinc(2*fc*intervalle);
+% H_passe_bas = abs(fftshift(fft(h)));
+% Freq = (-(2*ordre+1)/2:(2*ordre+1)/2-1)*(Fe/(2*ordre+1));
+% z = filter(H_passe_bas,[1],x_module);
+% Z = abs(fft(z));
+% plot(Freq,H_passe_bas);
 % plot(g, abs(fft(x_module)));
 % hold on;
 % plot(g, Z);
 %plot(T,z);
 
 %3.3.2 Filtre passe-haut
-H_passe_haut = 1 - H_passe_bas;
-y = filter(H_passe_haut,[1],x_module);
-figure;
-plot(Freq, H_passe_haut);
+% H_passe_haut = 1 - H_passe_bas;
+% y = filter(H_passe_haut,[1],x_module);
+% figure;
+% plot(Freq, H_passe_haut);
 % plot(T,y);
