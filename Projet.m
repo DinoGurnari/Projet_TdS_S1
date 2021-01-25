4f230f3d26d9bda6d32a93ee521b655f77051649
Pwu-Edu7 <58974222+Pwu-Edu7@users.noreply.github.com>
2021-01-25 03:08:04 +0100

    jpp ALED

--- Projet.m
+++ Projet.m
@@ -40,21 +40,24 @@ title('Signal NRZ(t) en fonction du temps');
 
 %3.1.1.3 Tracé de la DSP de NRZ théorique et expérimentale
 [DSP_NRZ_Th,f] = DSP_rectangulaire(Nb_echantillons,Fe,NRZ,0);
+DSP_NRZ_Exp = (Ts*sinc(pi*f*Ts).^2 + dirac(f))/4;
+
 figure; % figure 2
 subplot(211);
 semilogy(f,DSP_NRZ_Th);
+ylim([1e-06 1000]);
+xlim([-2000 2000]);
 xlabel('f en Hz');
 ylabel('DSP NRZ Th(f)');
 title('DSP théorique de NRZ en fonction de f');
 
-DSP_NRZ_Exp = (Ts*sinc(pi*f*Ts).^2 + dirac(f))/4;
-%figure;
 subplot(212);
 semilogy(f,DSP_NRZ_Exp);
+ylim([1e-10 1e-2]);
+xlim([-2000 2000]);
 xlabel('f en Hz');
 ylabel('DSP NRZ Exp(f)');
 title('DSP expérimentale de NRZ en fonction de f');
-
 % pwelch à tester plus tard
 
 %3.1.2.1 Génération du signal x(t) modulé en fréquence
@@ -63,7 +66,6 @@ phi1 = rand*2*pi;
 Cos0 = cos(2*pi*F0*T + phi0);
 Cos1 = cos(2*pi*F1*T + phi1);
 %Un = eye(size(NRZ));
-
 x_module = (1 - NRZ).*Cos0 + NRZ.*Cos1;
 
 %3.1.2.2 Tracé du signal x(t) en fonction du temps
@@ -84,11 +86,12 @@ title('Signal x(t) en fonction de t');
 
 %3.1.2.4 Tracé de la DSP de x(t) théorique et expérimentale
 DSP_X_Th = DSP_rectangulaire(Nb_echantillons,Fe,x_module,0);
+
 figure; % figure 4
 subplot(211);
-%ylim([1e-04 10]);
-%xlim([-6000 6000]);
 semilogy(f,DSP_X_Th);
+ylim([1e-05 100]);
+xlim([-7000 7000]);
 xlabel('f en Hz');
 ylabel('DSP X Th(f)');
 title('DSP X théorique en fonction de f');
@@ -143,28 +146,102 @@ title('Tracé du signal x bruite en fonction du temps pour SNR = 10');
 
 %3.3.1 Filtre passe-bas
 ordre = 60;
-g = Fe*(1:length(x_module))/length(x_module);
 fc = 4000; %Hz
-intervalle = (-ordre*Te:Te:ordre*Te);
-
-figure;
-h = (2*fc/Fe)*sinc(2*fc*intervalle);
-H_passe_bas = abs(fftshift(fft(h)));
-Freq = (-(2*ordre+1)/2:(2*ordre+1)/2-1)*(Fe/(2*ordre+1));
-z = filter(h,[1],x_module);
-Z = abs(fft(z));
-% plot(Freq,H_passe_bas);
-% plot(g, abs(fft(x_module)));
-% hold on;
-% plot(g, Z);
-plot(T,z);
+fnorm = fc/Fe;
+Tfiltre = (-ordre*Te:Te:ordre*Te);
+Ffiltre = (-(2*ordre+1)/2:(2*ordre+1)/2-1)*(Fe/(2*ordre+1));
+h_passe_bas = (2*fnorm)*sinc(2*fc*Tfiltre);
+y_passe_bas = filter(h_passe_bas,(1),x_module);
+H_passe_bas = abs(fftshift(fft(h_passe_bas)));
 
 %3.3.2 Filtre passe-haut
-H_passe_haut = 1 - H_passe_bas;
-h_passe_haut = - h;
+%H_passe_haut = 1 - H_passe_bas;
+h_passe_haut = - h_passe_bas;
 h_passe_haut(ordre+1) = h_passe_haut(ordre+1) + 1;
-y = filter(h_passe_haut,[1],x_module);
-figure;
+y_passe_haut = filter(h_passe_haut,(1),x_module);
+
+%3.3.3 Filtrage ok + manque prise en compte du retard
+
+%3.3.4.1 Tracés réponse impulsionnelle et réponse en fréquence des filtres
+figure; % figure 7 
+subplot(211);
+plot(Tfiltre,h_passe_bas);
+title('Réponse impulsionnelle du filtre passe bas');
+%ylim([-1.5 1.5]);
+%xlim([0.01 0.03]);
+
+subplot(212);
+plot(Ffiltre,H_passe_bas);
+title('Reponse fréquentielle du filtre passe bas');
+%ylim([-1.5 1.5]);
+%xlim([0.01 0.03]);
+
+%3.3.4.2 Tracés DSP de x(t) et y(t)
+DSP_Y_passe_bas = DSP_rectangulaire(Nb_echantillons,Fe,y_passe_bas,0);
+DSP_Y_passe_haut = DSP_rectangulaire(Nb_echantillons,Fe,y_passe_haut,0);
+
+figure; % figure 8 - DSP x(t) et y(t)
+subplot(311);
+semilogy(f,DSP_X_Th);
+ylim([1e-05 100]);
+xlim([-7000 7000]);
+xlabel('f en Hz');
+ylabel('DSP X Th(f)');
+title('DSP X théorique en fonction de f');
+
+subplot(312);
+semilogy(f,DSP_Y_passe_bas);
+ylim([1e-05 100]);
+xlim([-7000 7000]);
+xlabel('f en Hz');
+ylabel('DSP Y passe bas(f)');
+title('DSP Y passe bas en fonction de f');
+
+subplot(313);
+semilogy(f,DSP_Y_passe_haut);
+ylim([1e-05 100]);
+xlim([-7000 7000]);
+xlabel('f en Hz');
+ylabel('DSP Y passe haut(f)');
+title('DSP Y passe haut en fonction de f');
+
+% numéro à changer 
+figure; % figure 7 - passe bas
+subplot(211);
+plot(T,x_module);
+ylim([-1.5 1.5]);
+xlim([0.01 0.03]);
+
+subplot(212);
+plot(T,y_passe_bas);
+ylim([-1.5 1.5]);
+xlim([0.01 0.03]);
+
+figure; % figure 8 - passe haut
+subplot(211);
+plot(T,x_module);
+ylim([-1.5 1.5]);
+xlim([0.01 0.03]);
+
+subplot(212);
+plot(T,y_passe_haut);
+ylim([-1.5 1.5]);
+xlim([0.01 0.03]);
 % plot(Freq, H_passe_haut);
 % plot(intervalle, h_passe_haut);
-plot(T,y);
+
+figure; % figure 9 - passe haut et bas
+subplot(311);
+plot(T,x_module);
+ylim([-1.5 1.5]);
+xlim([0.01 0.03]);
+
+subplot(312);
+plot(T,y_passe_bas);
+ylim([-1.5 1.5]);
+xlim([0.01 0.03]);
+
+subplot(313);
+plot(T,y_passe_haut);
+ylim([-1.5 1.5]);
+xlim([0.01 0.03]);
